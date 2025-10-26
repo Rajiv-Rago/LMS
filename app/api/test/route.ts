367c8bd644cccd9ec5f6dbf984526e26cb6daf90
Rajiv <ragorajiv2@gmail.com>
2025-10-26 22:47:44 +0800

    Set up Next.js and MongoDB connection

---
+++ app/api/test/route.ts
@@ -0,0 +1,21 @@
+// app/api/test/route.ts
+
+import { dbConnect } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try{
+        let conn = await dbConnect();
+
+        return NextResponse.json({
+        status: "success",
+        host: conn.connection.host,
+        name: conn.connection.name,
+        readyState: conn.connection.readyState, // 1 means connected
+        });
+    }
+    catch (error){
+        return NextResponse.json({ error: 'Connection failed' }, { status: 500 });
+    }
+        
+}
